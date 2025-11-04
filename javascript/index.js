2aea4f6609336b1f3ee859dc144fe6b3d9957002
Iniobong Jones <74598414+inijones@users.noreply.github.com>
2025-11-04 12:03:47 +0100

    Update with Final Html and Css

---
+++ javascript/index.js
@@ -0,0 +1,50 @@
+console.log("This is Javascript");
+console.log(2 * 2);
+console.log("This works separately");
+
+let myName = "Alice";
+
+// let is a declaration keyword
+// myName is the variable name
+// = is the assign operator
+// "Alice" is the data of String type
+// ; is at the end of a line of code
+
+let age = 25;
+myName = "Feranmi";
+console.log(myName, age);
+
+const tutor = "Ini";
+console.log(tutor);
+
+let a = "Inijones";
+let b = 27;
+let c = true;
+let d;
+let e = "Something";
+e = null;
+
+console.log("The data type for a is " + typeof a);
+console.log("The data type for b is " + typeof b);
+console.log("The data type for c is " + typeof c);
+console.log("The data type for d is " + typeof d);
+console.log(e);
+
+let x = 10;
+
+// Comment
+/* */ //  Multiline Comment
+
+x = "Now I'm a string!";
+console.log(typeof x); // string
+
+// Objects
+let michael = {
+  name: "Michael",
+  age: 24,
+  height: "180cm",
+};
+
+michael.age = 54;
+
+console.log(michael);
